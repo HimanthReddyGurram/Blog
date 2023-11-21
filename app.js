512fb75e375367d9f197b6d52cfa430c1341b00f
himanthReddyGurram <kimhkimhk@gmail.com>
2023-11-21 15:33:05 +0530

    changed database name

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/dataDB")
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB")
 
 const postSchema = new mongoose.Schema({
   title: {type: String},
